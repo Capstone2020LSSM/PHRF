44d7b0126710e11d49f69de421e46974554af3cc
Seungmin Baek <yingking100@hanyang.ac.kr>
2020-08-21 14:25:12 +0900

    edit markdown and etc

---
+++ ing/nodebird/routes/page.js
@@ -0,0 +1,25 @@
+const sexpress = require('express');
+
+const router = express.Router();
+
+router.get('/profile', (req, res) => {
+  res.render('profile', { title: '내 정보 - NodeBird', user: null });
+});
+
+router.get('/join', (req, res) => {
+  res.render('join', {
+    title: '회원가입 - NodeBird',
+    user: null,
+    joinError: req.flash('joinError'),
+  }),
+});
+
+router.get('/', (req, res, next) => {
+  res.render('main', {
+    title: 'NodeBird',
+    twits: [],
+    loginError: req.flash('loginError'),
+  });
+});
+
+module.exports = router;
